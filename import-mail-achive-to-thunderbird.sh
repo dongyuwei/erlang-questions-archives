6afbf5025fbbd02daf6ecdc6c9add19ca9fd70aa
Dong Yuwei <newdongyuwei@gmail.com>
2016-12-12 15:07:34 +0800

    force recreate

--- import-mail-achive-to-thunderbird.sh
+++ import-mail-achive-to-thunderbird.sh
@@ -1,5 +1,5 @@
 cd ./archives
-gunzip *.gz
+gunzip -f *.gz
 for i in $(ls *.txt); do cat $i >> erlang-questions-archives; done
 # cp erlang-questions-archives ~/.thunderbird/*.default/Mail/Local\ Folders
 cp erlang-questions-archives ~/Library/Thunderbird/Profiles/cacug0f3.default/Mail/Local\ Folders/
